efe5e1c72eea43c63d98e0210955196a72dfae63
Emilio Murta <emilio.murta@msitbrasil.com>
2020-04-21 18:45:41 -0300

    add mask and validation

--- src/agenda/components/Checkout/index.tsx
+++ src/agenda/components/Checkout/index.tsx
@@ -25,8 +25,9 @@ interface Props {
     onReturn: (() => void);
 }
 interface State {
-    agenda: AgendaModel,
-    error: any
+    agenda: AgendaModel | null,
+    customer: Customer | null,
+    errors: Record<string, Array<string>>
 }
 
 export default class Checkout extends React.PureComponent<Props, State> {
@@ -36,6 +37,7 @@ export default class Checkout extends React.PureComponent<Props, State> {
 
     constructor(props: Props) {
         super(props);
+        this.state = { agenda: null, customer: null, errors: {} }
         this.nameInput = React.createRef();
         this.phoneInput = React.createRef();
         this.emailInput = React.createRef();
@@ -66,7 +68,19 @@ export default class Checkout extends React.PureComponent<Props, State> {
             });
 
         fetch(request)
-        .then(res => res.json())
+        .then(res => {
+            if(res.status === 400 || res.status === 500) {
+                res.json().then(errors => {
+                    this.setState({
+                        customer: appointment.customer,
+                        errors
+                    });
+                });
+                throw new Error("Skipping other stuff, return has status code invalid!");
+            }
+
+            return res.json();
+        })
         .then(
             (result: AgendaModel) => {
                 this.setState({
@@ -77,11 +91,31 @@ export default class Checkout extends React.PureComponent<Props, State> {
             // instead of a catch() block so that we don't swallow
             // exceptions from actual bugs in components.
             (error) => {
-                this.setState({
-                    error
-                });
+                console.log(error);
             }
         )
+    }
+
+    justMask = (event: React.FormEvent<HTMLInputElement>) => {
+        const userInputVal = event.currentTarget.value;
+        let cleaned = ('' + userInputVal).replace(/\D/g, '');
+        let masked = event.currentTarget.value;
+        
+        //Check if the input is of correct length
+        let matchHomePhone = cleaned.match(/^(\d{2})(\d{4})(\d{4})$/);
+        let matchCelPhone = cleaned.match(/^(\d{2})(\d{5})(\d{4})$/);
+
+        if (matchHomePhone) {
+            masked = '(' + matchHomePhone[1] + ') ' + matchHomePhone[2] + '-' + matchHomePhone[3]
+        };
+
+        if (matchCelPhone) {
+            masked = '(' + matchCelPhone[1] + ') ' + matchCelPhone[2] + '-' + matchCelPhone[3]
+        };
+
+        console.log(event.currentTarget.value, masked);
+        event.currentTarget.value = masked;
+        return null;
     }
 
     render() {
@@ -112,21 +146,50 @@ export default class Checkout extends React.PureComponent<Props, State> {
                         <Form.Label>Nome Completo</Form.Label>
                         <Form.Control type="text" 
                             placeholder="Digite seu nome..." 
+                            isValid={this.state.customer !== null && !this.state.errors['name']}
+                            isInvalid={this.state.customer !== null && this.state.errors['name'] != null && this.state.errors['name'].length > 0}
                             ref={this.nameInput} />
+                        { 
+                            this.state.errors
+                            && this.state.errors['name'] 
+                            && this.state.errors['name'].map(err => (<Form.Control.Feedback type="invalid">
+                                {err}
+                            </Form.Control.Feedback>)) 
+                        }
                     </Form.Group>
 
                     <Form.Group controlId="frmGrpPhone">
                         <Form.Label>Telefone</Form.Label>
                         <Form.Control type="text"
-                            placeholder="(ddd)99999-9999" 
+                            placeholder="(99) 99999-9999"
+                            maxLength={15}
+                            onChange={(evt: any) => this.justMask(evt)}
+                            isValid={this.state.customer !== null && !this.state.errors['phone']}
+                            isInvalid={this.state.customer !== null && this.state.errors['phone'] != null && this.state.errors['phone'].length > 0}
                             ref={this.phoneInput} />
+                        { 
+                            this.state.errors
+                            && this.state.errors['phone'] 
+                            && this.state.errors['phone'].map(err => (<Form.Control.Feedback type="invalid">
+                                {err}
+                            </Form.Control.Feedback>)) 
+                        }
                     </Form.Group>
 
                     <Form.Group controlId="frmGrpEmail">
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="email" 
                             placeholder="Digite seu e-mail..." 
+                            isValid={this.state.customer !== null && !this.state.errors['email']}
+                            isInvalid={this.state.customer !== null && this.state.errors['email'] != null && this.state.errors['email'].length > 0}
                             ref={this.emailInput} />
+                        { 
+                            this.state.errors
+                            && this.state.errors['email'] 
+                            && this.state.errors['email'].map(err => (<Form.Control.Feedback type="invalid">
+                                {err}
+                            </Form.Control.Feedback>)) 
+                        }
                     </Form.Group>
                     <Button variant="primary" type="submit" onClick={this.submitForm}>
                         Agendar!
